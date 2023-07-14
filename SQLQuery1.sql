ab870188ea4b5afc048a99fbd75fa70bd552863d
Poojachakor <chakorpooja07@gmail.com>
2023-07-14 22:54:41 +0530

    [Pooja]Add:AddMoreDetailsInTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,9 @@ Gender char, Country varchar(200))
 
 
 select * from Customer
+Insert into Customer (Name, Gender, Country) Values ('Vishesh', 'M', 'India')
+Insert into Customer (Name, Gender, Country) Values ('Pooja', 'F', 'Germany')
+Insert into Customer (Name, Gender, Country) Values ('Rupesh', 'M', 'Uk')
+Insert into Customer (Name, Gender, Country) Values ('Pallavi', 'F', 'India')
+Insert into Customer (Name, Gender, Country) Values ('Shrdha', 'F', 'Canada')
+
